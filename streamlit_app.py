7f2bb56bf4413bb65aa916b8234302a7b5707025
omar mohamed <275111862+omar-mohamed-CS@users.noreply.github.com>
2026-05-20 00:15:43 +0300

    Update model path for GenerativeModel initialization

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,7 @@ st.set_page_config(
 # =========================
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
-
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 # =========================
 # قراءة ملف الـ PDF
 # =========================
