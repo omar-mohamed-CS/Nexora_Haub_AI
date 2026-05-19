6852a5ef017c1affad8c86d8527700788c57ee87
omar mohamed <275111862+omar-mohamed-CS@users.noreply.github.com>
2026-05-19 23:52:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.set_page_config(
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # =========================
 # قراءة ملف الـ PDF
